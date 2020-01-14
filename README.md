0e8d182b55590e788fd6fe9dada3a049443799c8
andrewtr218 <andrewtr218@gmail.com>
2020-01-14 17:20:29 -0600

    added functionality from 5 day forecast, still can't get picture to work. Page Format OK. Added TODO and Questions in README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # weatherdashboard
-I cannot complete this currently because I have to prepare for a whole trial review process tomorrow regarding Project 1. Hope to update soon.
+Shows current weather in london as well as 5 day forecast.
+
+To Do:
+Add button and functionality for user to enter a custom city input
+
+Q's:
+1. How do you add something to local storage?
+2. How do I import the the icons from the weather API?
+3. How do I store previously searched cities in localstorage and display them to the user?
+4. How to load last searched city forecast on page load
--- script.js
+++ script.js
@@ -39,7 +39,30 @@ $.ajax({
     url: "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + Location + "&appid=36fbb44d644fb8f6ba6572a1c00e22c9",
     method: "GET"
 }).then(function(data){
-    console.log(data)
+    $(".dateDay1").text(`${data.list[4].dt_txt}`);
+    // $(".iconDay1").attr("src", `${data.list[0].weather[0].icon}`)
+    $("#tempDay1").text(`Temperature: ${data.list[4].main.temp} Kelvin`);
+    $("#humidDay1").text(`Humidity: ${data.list[4].main.humidity}`)
+    
+    $(".dateDay2").text(`${data.list[12].dt_txt}`);
+    // $(".iconDay1").attr("src", `${data.list[0].weather[0].icon}`)
+    $("#tempDay2").text(`Temperature: ${data.list[12].main.temp} Kelvin`);
+    $("#humidDay2").text(`Humidity: ${data.list[12].main.humidity}`)
+    
+    $(".dateDay3").text(`${data.list[20].dt_txt}`);
+    // $(".iconDay1").attr("src", `${data.list[0].weather[0].icon}`)
+    $("#tempDay3").text(`Temperature: ${data.list[20].main.temp} Kelvin`);
+    $("#humidDay3").text(`Humidity: ${data.list[20].main.humidity}`)
+
+    $(".dateDay4").text(`${data.list[28].dt_txt}`);
+    // $(".iconDay1").attr("src", `${data.list[0].weather[0].icon}`)
+    $("#tempDay4").text(`Temperature: ${data.list[28].main.temp} Kelvin`);
+    $("#humidDay4").text(`Humidity: ${data.list[28].main.humidity}`)
+
+    $(".dateDay5").text(`${data.list[36].dt_txt}`);
+    // $(".iconDay1").attr("src", `${data.list[0].weather[0].icon}`)
+    $("#tempDay5").text(`Temperature: ${data.list[36].main.temp} Kelvin`);
+    $("#humidDay5").text(`Humidity: ${data.list[36].main.humidity}`)
 })
 };
 
